af51cf9d06dd3d3929b6bee25d510cecbaafea4f
Dylan Stancil <76175348+Ihce@users.noreply.github.com>
2022-12-13 22:03:38 -0600

    Delete sbom directory

--- sbom/info.py
+++
@@ -1,4 +0,0 @@
-def info():
-    """ Return developer id """
-    DEVELOPER = 'dbs0025'
-    return DEVELOPER
--- sbom/test/sbomTest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import sbom.info as sbom
-
-class SbomTest(unittest.TestCase):
-
-    def test_sbom_010_shouldReturnDeveloperName(self):
-        myName = 'dbs0025'
-        self.assertIn(myName, sbom.info())
